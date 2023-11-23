b7aa25bd33f584fef311f4f051ae88212f57871a
scott-fisher894 <130784031+scott-fisher894@users.noreply.github.com>
2023-11-22 21:16:29 -0800

    Implemented Movement and Move Validation

--- AlphaMain.py
+++ AlphaMain.py
@@ -1,7 +1,6 @@
 
 import pygame
 import sys
-
 from Board import *
 
 
@@ -9,12 +8,18 @@ class Main:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT + 50))
         pygame.display.set_caption('AlphaCheck')
         self.game = Game()
+        self.font = pygame.font.Font(None, 36)  # Choose appropriate font
+
+    def draw_text_box(self, text):
+        text_box_rect = pygame.Rect(0, HEIGHT, WIDTH, 50)  # Position and size of the text box
+        self.screen.fill((0, 0, 0), text_box_rect)  # Fill with background color
+        text_surface = self.font.render(text, True, (255, 255, 255))  # Render the text
+        self.screen.blit(text_surface, (10, HEIGHT + 10))  # Adjust text position as needed
 
     def mainloop(self):
-
         screen = self.screen
         running = True
 
@@ -26,8 +31,11 @@ class Main:
                     running = False
                     pygame.quit()
                     sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.game.handle_mouse_event(event)
             
-            
+            self.game.show_board(screen)
+            self.draw_text_box(self.game.get_status_text())  # Draw the status text
             pygame.display.update()
 
     
--- Board.py
+++ Board.py
@@ -10,6 +10,7 @@ class Game:
     def __init__(self):
         self.board = chess.Board()
         self.images = {}
+        self.selected_square = None
         self.load_images()
 
     def load_images(self):
@@ -43,9 +44,37 @@ class Game:
                 piece_image_key = ('w' if piece.color == chess.WHITE else 'b') + symbol
                 if piece_image_key in self.images:
                     x = (i % 8) * SQ_SIZE
-                    y = (i // 8) * SQ_SIZE
+                    y = (7 - (i // 8)) * SQ_SIZE
                     # Centering the piece in the square
                     piece_image = self.images[piece_image_key]
                     surface.blit(piece_image, (x + (SQ_SIZE - piece_image.get_width()) // 2,
                                             y + (SQ_SIZE - piece_image.get_height()) // 2))
-                    
+
+    def handle_mouse_event(self, event):
+        if event.button == 1:  # Left mouse button
+            pos = pygame.mouse.get_pos()
+            col = pos[0] // SQ_SIZE
+            row = DIMENSION - 1 - (pos[1] // SQ_SIZE)
+            square = chess.square(col, row)
+            print(f"Clicked on pixel {pos}, which is column {col}, row {row}, square {square}")
+
+            if self.selected_square is None:
+                piece = self.board.piece_at(square)
+                if piece and piece.color == self.board.turn:
+                    self.selected_square = square
+            else:
+                move = chess.Move(self.selected_square, square)
+                if move in self.board.legal_moves:
+                    self.board.push(move)
+                    self.selected_square = None  # Reset selection after move
+                else:
+                    self.selected_square = None  # Deselect if move is illegal
+
+    def get_status_text(self):
+        if self.board.is_checkmate():
+            return "Checkmate!"
+        elif self.board.is_check():
+            return "Check!"
+        else:
+            turn_text = "White's Turn" if self.board.turn == chess.WHITE else "Black's Turn"
+            return turn_text
